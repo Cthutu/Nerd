085128ab2ae5b38cba75199e527edf080db5439f
Matt Davies <mattie.davies@gmail.com>
2018-10-09 16:24:45 -0400

    Fixed install.bat so it doesn't bring in too many files.

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
 if [%INSTALL_PATH%] NEQ [] (
     copy /y _bin\Win64_Release_nd\nd.exe %INSTALL_PATH%
-    copy /y _bin\Win64_Release_nd\*.n %INSTALL_PATH%
+    copy /y _bin\Win64_Release_nd\system.n %INSTALL_PATH%
 ) else (
     echo Please define INSTALL_PATH in the environment
 )
